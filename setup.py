6d44a85d722bfa5765b2d16a68d002726508daa0
rubensdmp <rubensdmp@gmail.com>
2022-05-26 19:44:52 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 HERE = pathlib.Path(__file__).parent
 
-VERSION = '0.0.1' 
+VERSION = '0.0.2' 
 PACKAGE_NAME = 'stratilib'
 AUTHOR = 'Rubens Merlino' 
 AUTHOR_EMAIL = 'rubensdmp@gmail.com' 
